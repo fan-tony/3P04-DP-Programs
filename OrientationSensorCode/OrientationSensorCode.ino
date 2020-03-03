2888d88fba6a8b37855d8355497b0a744a4bd4c1
fant6 <tonyfan1013@gmail.com>
2020-03-03 16:44:05 -0500

    rather than comparing each acceleration vector, the absolute acceleration is compared to determine if the person is in motion. Also, it now prints out the angle of the sensor when moving , and nothing when not moving, instead of 'DATA OUTGOING' or 'PROGRAM STOPPED'

--- OrientationSensorCode/OrientationSensorCode.ino
+++ OrientationSensorCode/OrientationSensorCode.ino
@@ -22,15 +22,15 @@ Adafruit_BNO055 bno2; //initialize sensor object2; this has the changed sda and 
 /******************
 * Global variables*
 ******************/
-double stop1[3]={0,0,0};//standing still acceleration of x y and z
-double stop2[3]={0,0,0};
+double stop1 = 0;//standing still acceleration of x y and z
+double stop2 = 0;
 int stopFlag = 0;
 
 void setup(void){
   //Initial Setup
   Serial.begin(9600);//begin serial information
   SerialBT.begin("ESP32Team5");//search your bluetooth devices for ESP32 and connect once the esp is powered and running the code
-  SerialBT.println("Begin Orientation Sensor test");
+  SerialBT.println("");//print header for CSV file
 
   Wire.begin();
   Wire1.begin(SDA_2, SCL_2);
@@ -61,21 +61,17 @@ void setup(void){
     bno1.getEvent(&stopEvent1,Adafruit_BNO055::VECTOR_LINEARACCEL);//get the event occuring and store it in event 1
     bno2.getEvent(&stopEvent2,Adafruit_BNO055::VECTOR_LINEARACCEL);//store the event from sensor 2 in event2's address
   
-    stop1[0]+=stopEvent1.acceleration.x;
-    stop1[1]+=stopEvent1.acceleration.y;
-    stop1[2]+=stopEvent1.acceleration.z;
-    stop2[0]+=stopEvent2.acceleration.x;
-    stop2[1]+=stopEvent2.acceleration.y;
-    stop2[2]+=stopEvent2.acceleration.z;
+    stop1 = stop1 + ((abs(stopEvent1.acceleration.x) + abs(stopEvent1.acceleration.y) + abs(stopEvent1.acceleration.z))/3);//
+    stop2 = stop2 + ((abs(stopEvent2.acceleration.x) + abs(stopEvent2.acceleration.y) + abs(stopEvent2.acceleration.z))/3);
     delay(50);
   }
   
-  for (int i=0;i<3;i++){//average them out now because 100 samples were just taken
-    stop1[i]/=100;
-    stop1[i]+=0.05;//add a small buffer zone for stopping
-    stop2[i]/=100;
-    stop2[i]+=0.05;//add a small buffer zone for stopping
-  }
+  //average them out now because 100 samples were just taken
+  stop1/=100;
+  stop1+=0.05;//add a small buffer zone for stopping
+  stop2/=100;
+  stop2+=0.05;//add a small buffer zone for stopping
+  
 }
 
 /**********************************************
@@ -88,19 +84,10 @@ void getOrientation(sensors_event_t event, double orientation[]){
   orientation[2] = (event.orientation.z);
  }
 
-void infoToString(double info[3]){//info is expressed as a double array where each array holds 3 elements denoting x, y, and z
-  SerialBT.print("X: ");
-  SerialBT.print(info[0], 4);
-  SerialBT.print("\tY: ");
-  SerialBT.print(info[1], 4);
-  SerialBT.print("\tZ: ");
-  SerialBT.print(info[2], 4);
-  SerialBT.println("");
-}
 
 boolean checkStop(){//gets the current acceleration, and compares it to the average stopping acceleration
-  double accel1[3];
-  double accel2[3];
+  double accel1 = 0;
+  double accel2 = 0;
   sensors_event_t stopEvent1;//initialize event as a sensor event for sensor object 1
   sensors_event_t stopEvent2;//initialize event as a sensor event for sensor object 2
 
@@ -108,36 +95,25 @@ boolean checkStop(){//gets the current acceleration, and compares it to the aver
   bno1.getEvent(&stopEvent1, Adafruit_BNO055::VECTOR_LINEARACCEL);//get the event occuring and store it in event 1
   bno2.getEvent(&stopEvent2, Adafruit_BNO055::VECTOR_LINEARACCEL);//store the event from sensor 2 in event2's address
   
-  accel1[0]+=stopEvent1.acceleration.x;
-  accel1[1]+=stopEvent1.acceleration.y;
-  accel1[2]+=stopEvent1.acceleration.z;
-  accel2[0]+=stopEvent2.acceleration.x;
-  accel2[1]+=stopEvent2.acceleration.y;
-  accel2[2]+=stopEvent2.acceleration.z;
+  accel1 = accel1 + ((abs(stopEvent1.acceleration.x) + abs(stopEvent1.acceleration.y) + abs(stopEvent1.acceleration.z))/3);
+  accel2 = accel2 + ((abs(stopEvent2.acceleration.x) + abs(stopEvent2.acceleration.y) + abs(stopEvent2.acceleration.z))/3);
 
-  SerialBT.print("X: ");
-  SerialBT.print(stop1[0]);
-  SerialBT.print(" Y: ");
-  SerialBT.print(stop1[1]);
-  SerialBT.print(" Z: ");
-  SerialBT.print(stop1[2]);
-  SerialBT.println("");
-
-  SerialBT.print("X: ");
-  SerialBT.print(abs(accel2[0]));
-  SerialBT.print(" Y: ");
-  SerialBT.print(abs(accel2[1]));
-  SerialBT.print(" Z: ");
-  SerialBT.print(abs(accel2[2]));
-  SerialBT.println("");
-
-  if(abs(accel1[0]) < stop1[0]  || abs(accel1[1]) < stop1[1] || abs(accel1[2]) < stop1[2] || abs(accel2[0]) < stop2[0] || abs(accel2[1]) < stop2[1] || abs(accel2[2]) < stop2[2]){//if any acceleration values are under the average stopping, then stop sending data 
-    return true;
+  if(accel1<stop1 || accel2<stop2){//if current accleration doesn't exceed threshold of stopping
+    return true;//you are stopped and not in motion
     }
     return false;//otherwise you are in motion
   }
   
 
+void infoToString(double info[3]){//info is expressed as a double array where each array holds 3 elements denoting x, y, and z
+  //SerialBT.print("X: ");
+  //SerialBT.print(info[0], 4);
+  //SerialBT.print("\tY: ");
+  //SerialBT.print(info[1], 4);
+  //SerialBT.print("\tZ: ");
+  SerialBT.println(info[2], 4);//prints only the z component of the change in angle
+  //SerialBT.println("");
+}
 /********************************
  * Main method to be implemented*
  *******************************/
@@ -165,21 +141,18 @@ void loop(void){
   }
   
   //print them out to verify 
-  //infoToString(angle);
-  SerialBT.println("DATA OUTGOING");
-
+  infoToString(angle);
 
 
   if (checkStop()==true){stopFlag +=1;}//check if the person is standing still 
   if(stopFlag==50){//if the person has been standing still for 5 second (50 * 100ms delay at the end of the loop)
     while(checkStop()==true){
     //then do nothing, until the program reads that they're moving again
-    SerialBT.println("PROGRAM STOPPED");
-    delay(100);
+    delay(50);
     }
   stopFlag=0;//if you're out here then you've started moving again, so reset the stopFlag counter
   }
   
-  delay(100);//100ms wait time between each reading
+  delay(50);//100ms wait time between each reading
 
 }
